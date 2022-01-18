b646f10f87ce554d17285f35148280e0fce59683
Suresh Sharma <shasures@sheridancollege.ca>
2022-01-18 13:20:55 -0500

    First Commit

---
+++ src/personlist/Person.java
@@ -0,0 +1,41 @@
+package personlist;
+
+
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Suresh Sharma
+ */
+public class Person {
+    private String p_name;
+    private String p_address;
+
+    /**
+     * @return the p_name
+     */
+    public String getP_name() {
+        return p_name;
+    }
+
+    /**
+     * @param p_name the p_name to set
+     */
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    /**
+     * @return the p_address
+     */
+    public String getP_address() {
+        return p_address;
+    }
+
+    /**
+     * @param p_address the p_address to set
+     */
+    public void setP_address(String p_address) {
+        this.p_address = p_address;
+    }
+}
---
+++ src/personlist/PersonList.java
@@ -0,0 +1,25 @@
+package personlist;
+
+import java.util.Scanner;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Suresh Sharma
+ */
+public class PersonList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+    Person [] p = new Person[3];
+    for(int i = 0; i<p.length; i++){
+            System.out.println("insert person name: ");
+            p[i].setP_name(input.nextLine());
+            
+}
+    }
+
+}
